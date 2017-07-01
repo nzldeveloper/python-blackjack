183babf05fb89e6550fb31eb5c6db04be4009baf
nzldeveloper <yohinachan2000@gmail.com>
2017-06-30 21:37:41 -0400

    complete blackjack game

--- blackjack.py
+++ blackjack.py
@@ -5,6 +5,8 @@
 import random
 
 bet = 0
+points = {'1':11, '2':2, '3':3, '4':4, '5':5, '6':6, '7':7, '8':8, '9':9, '0':10, 'J':10, 'Q':10, 'K':10 }
+winner = False
 
 class Player(object):
     def __init__(self, money=100):
@@ -18,8 +20,6 @@ class Player(object):
 
 class Cards(object):
     suits = ["Spades", "Hearts", "Diamond", "Clubs"]
-
-    #points = {'1':1, '2':2, '3':3, '4':4, '5':5, '6':6, '7':7, '8':8, '9':9 }
     
     def create_deck(self):
         card_deck = [] 
@@ -27,15 +27,6 @@ class Cards(object):
             for x in range(1,14):
                 card_deck.append(item+str(x))   
         return card_deck
-
-#generate new deck instance
-cards_instance = Cards()
-deck = cards_instance.create_deck()
-print(cards_instance.create_deck())
-
-#creating a new player instance
-player = Player()
-
 
 #Ask player to make bet before game starts
 def make_bet():
@@ -52,11 +43,11 @@ def make_bet():
 
 #function for player to input decision - hit or stand
 def playerDecision():
-    decision = (input('Do you want to hit (H) or stand (S)? ')).upper()
-    while decision != 'S' and decision !='H':
-        decision = (input('Please hit (H) or stand (S) ')).upper()
-    else:
-        return decision
+  decision = (input('Do you want to hit (H) or stand (S)? ')).upper()
+  while decision != 'S' and decision !='H':
+    decision = (input('Please hit (H) or stand (S) ')).upper()
+  else:
+    return decision
 
 #pick a random number
 def randomize():
@@ -72,57 +63,152 @@ def display_hand(cards_in_hand):
         elif cards_in_hand[idx][-2:] == '13':
             cards_in_hand[idx] = cards_in_hand[idx][:-2] + 'K'
 
-#function to calculate player and dealer's score right after two cards are dealt to see if anyone has 21
-#paramters - pass in either the dealer's hand or the player's hand
-#card_one = player_hand[0]
-#card_two = player_hand[1]
-def score_after_deal(card_one, card_two):
-    if card_one[-1] == '1' and (card_two[-1] == 'J' or card_two[-1] == 'Q' or card_two[-1] == 'K' or card_two[-2] == '10'):
-        total_points = 21
-        return total_points
-    elif card_two[-1] == '1' and (card_one[-1] == 'J' or card_one[-1] == 'Q' or card_one[-1] == 'K' or card_one[-2] == '10'):
-        total_points = 21
-        return total_points
-    else:
-        return "Not 21."
+#calculate score
+#parameter - pass in either player_hand or dealer_hand
+def calc_score(cards):
+  sum = 0
+  last_number=[item[-1] for item in cards]
+  #if the last number is 0, then that's a 10 point card
+  for item in last_number:
+    sum += points[item]
+  try:
+    if last_number.index('1') >= 0 and sum > 21:
+      sum = sum - 11*last_number.count('1') + 1*last_number.count('1')
+      #11 and 1 are multipled by the number of times the player draws an Ace; in case player draws a second or third ace
+  except:
+    pass
+  return sum
+  
+#checks if dealer has to draw (less than 17 in hand)
+def dealer_draw():
+  global dealer_score
+  dealer_hand.append(deck[randomize()])
+  deck.remove(dealer_hand[-1])
+  display_hand(dealer_hand)
+  dealer_score = calc_score(dealer_hand)
+  return dealer_score
+
+#if player or deal is dealt 21 on the first 2 cards
+def natural_win():
+  if player_score == 21 and dealer_score != 21:
+    player.money = player.addMoney()
+    print('You win! Your money increase to: {} '.format(player.money))
+  elif dealer_score == 21 and player_score != 21:
+    player.money = player.loseMoney()
+    print('You lost this round. Your money decrease to: {}'.format(player.money))
+  elif dealer_score == 21 and player_score == 21:
+    print("It's a tie. You did not win or lose any money")
+
+#check winning conditions
+def win_condition():
+  if dealer_score > 21:
+    player.money = player.addMoney()
+    print('Dealer busts! Player wins! Player money increase to: {} '.format(player.money))
+  elif player_score > 21:
+    player.money = player.loseMoney()
+    print('Player busts! Dealer wins! Your money decrease to: {}.'.format(player.money))
+  elif dealer_score == player_score:
+    print("It's a tie. Player wins no money")
+  elif dealer_score>player_score:
+    player.money = player.loseMoney()
+    print('Dealer wins! Your money decrease to: {}.'.format(player.money))
+  elif player_score>dealer_score:
+    player.money = player.addMoney()
+    print('Player wins! Player money increase to: {} '.format(player.money))
+
+def display_score():
+  print('Player score is ', player_score)
+  print('Dealer score is ', dealer_score)
+
+#function to deal cards; takes in a num that indicates how many cards to deal and which person (player or dealer)
+def deal_cards(num, hand):
+  i=0
+  while i<num:
+    hand.append(deck[randomize()])
+    deck.remove(hand[-1])
+    i+=1
+  return hand
+
+#asks if the player wants to play again
+def ask_play():
+  ask_play = input('Do you want to play again? (Y/N)').upper()
+  while ask_play != 'Y' and ask_play !='N':
+    ask_play = (input('Please hit (Y) or stand (N) ')).upper()
+  else:
+    return ask_play
+  
+#generate new deck instance
+cards_instance = Cards()
+deck = cards_instance.create_deck()
+
+#creating a new player instance
+player = Player()
 
 #######
 ##GAME CODE STARTS HERE! ABOVE ARE ONLY FUNCTIONS
 #######
 
-make_bet()
+while player.money > 0: 
+  player_hand = []
+  dealer_hand = []
+  make_bet()
 
-#deals out card (removes each card from the deck list as it is dealt so no dubplicate cards will be dealt)
-player_hand = [deck[randomize()]]
-deck.remove(player_hand[0])
-dealer_hand = [deck[randomize()]]
-deck.remove(dealer_hand[0])
-player_hand.append(deck[randomize()])
-deck.remove(player_hand[1])
-dealer_hand.append(deck[randomize()])
-
-#display player and dealer's hand
-display_hand(player_hand)
-display_hand(dealer_hand)
-print('Your hand is ', player_hand)
-print("Dealer's face-up card is", dealer_hand[0]) #player only sees one card at the beginning
-
-#right after 2 cards are dealt, check if anyone won off the bat.
-#if player has 21, (a ten and an ace), player wins
-#if dealer has 21, (a ten and an ace), dealer wins
-#if both player and dealer has 21, tie; player loses no money
-
-player_score = score_after_deal(player_hand[0], player_hand[1])
-dealer_score = score_after_deal(dealer_hand[0], dealer_hand[1])
-
-if player_score == 21 and dealer_score != 21:
-    player.money = player.addMoney()
-    print('You win! Your money increase to: ', player.money)
-elif dealer_score == 21 and player_score != 21:
-    player.money = player.loseMoney()
-    print('You lost this round. You money decrease to: ', player.money)
-elif dealer_score == 21 and player_score == 21:
-    print("It's a tie. You did not win or lose any money")
-else:
+  while bet > player.money: 
+      try: 
+        bet = int(input("You don't have that much money. Please enter an amount less than or equal to {}".format(player.money)))
+      except:
+        print('Please enter a number.')
+        continue
+  
+  player_hand = deal_cards(2, player_hand)
+  dealer_hand = deal_cards(2, dealer_hand)
+  
+  #display player and dealer's hand
+  display_hand(player_hand)
+  display_hand(dealer_hand)
+  print('Your hand is ', player_hand)
+  print("Dealer's face-up card is", dealer_hand[0]) #player only sees one card at the beginning
+  print("Dealer's face-down card is", dealer_hand[1])
+  
+  #right after 2 cards are dealt, check if anyone won off the bat.
+  #if player has 21, (a ten and an ace), player wins
+  #if dealer has 21, (a ten and an ace), dealer wins
+  #if both player and dealer has 21, tie; player loses no money
+  
+  player_score = calc_score(player_hand)
+  dealer_score = calc_score(dealer_hand)
+  
+  if player_score == 21 or dealer_score == 21: 
+    natural_win()
+    if player.money > 0:
+        play_again = ask_play()
+        if play_again == 'N':
+          break
+        else:
+            continue
+  ##below code runs if neither player or dealer have 21 from first 2 cards
+  else:
     decision = playerDecision()
-    print('Player decision is ', decision)
+    while decision != 'S':
+      print('Player draws another card')
+      player_hand = deal_cards(1, player_hand) #draws 1 card for the player
+      display_hand(player_hand)
+      player_score = calc_score(player_hand)
+      print('Player hand is ', player_hand)
+      if player_score > 21:
+        win_condition()
+        break
+      decision = playerDecision()
+    else: 
+      print('Player decides to Stand')
+      while dealer_score < 17:
+        dealer_draw()
+      display_score()
+      win_condition()
+
+  if player.money > 0:
+      play_again = ask_play()
+      if play_again == 'N':
+          break
+
+    
